a765b7ccd6b1c7ec8632ce47232bb09f0bd902a7
Pablo Jimeno <proyectos@sonicbyte.com>
2011-12-03 08:10:29 +0100

    Aliveness and death is everywhere

--- init_spec.rb
+++ init_spec.rb
@@ -2,35 +2,72 @@ require	'rubygems'
 require 'rspec'
 
 class Cell
+
+	def initialize(alive)
+		@alive = alive
+	end
+
 	def self.alive
-		self.new
+		self.new(true)
 	end
 
 	def self.dead
-		
+		self.new(false)
+	end
+
+	def evolve(live_neighbors)
+		@alive = live_neighbors == 2
+	end
+
+	def alive?
+		@alive
 	end
 end
 
 class Population
+
+	def initialize(grid)
+		@grid = grid
+	end
+
 	def self.with(grid)
-		self.new
+		self.new(grid)
 	end
 
 	def evolve
-		
+		center_cell.evolve(live_neighbors_of(center_cell))
+	end
+
+	def live_neighbors_of(cell)
+		alive_cells = @grid.flatten.select { |each| each.alive? }
+		alive_cells.size - (cell.alive? ? 1 : 0)
+	end
+
+	def center_cell
+		@grid[1][1]		
 	end
 end
 
 
 describe "Evolving only the center cell" do
-	def Alive
+	def alive
 		Cell.alive
+	end
+
+	def dead
+		Cell.dead
 	end
 
 	it	"stays alive with two live neighbors" do
 		center_cell = Cell.alive
 		Population.with([[alive, alive, dead], [dead, center_cell, dead], [dead, dead, dead]]).evolve
 		center_cell.should be_alive
+	end
+
+	it	"dies with four live neighbors" do
+		center_cell = Cell.alive
+		Population.with([[alive, alive, alive], [alive, center_cell, dead], [dead, dead, dead]]).evolve
+		center_cell.should_not be_alive
 	end
 
 end
